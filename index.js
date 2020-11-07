49a458e8e428560e276a6a426645cbec6bece633
amir naderi <63848506+amir-ndr@users.noreply.github.com>
2020-11-07 19:59:44 +0330

    data2

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-tetrahedral-64853',
-    user : 'postgres',
-    password : 'test',
-    database : 'celeb'
+    host :process.env.DATABASE_URL,
+    ssl=true,
   }
 });
 
