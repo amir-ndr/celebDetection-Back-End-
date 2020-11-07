e1d751a7e794a5494e853d6b7b6a772c88d52df0
amir naderi <63848506+amir-ndr@users.noreply.github.com>
2020-11-07 12:07:43 +0330

    first commit

---
+++ back-End/index.js
@@ -0,0 +1,108 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'celeb'
+  }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/',(req, res)=>{
+     res.send('it is working');
+})
+
+app.post('/SignIn', (req, res) => {
+    const {email,password}=req.body;
+    if(!email || !password){
+        return res.status(400).json('incorrect form submission');
+    }
+    db.select('email', 'hash').from('login')
+      .where('email', '=', email)
+      .then(data => {
+        const isValid = bcrypt.compareSync(password, data[0].hash);
+        if (isValid) {
+          return db.select('*').from('users')
+            .where('email', '=', email)
+            .then(user => {
+              res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+          res.status(400).json('wrong credentials')
+        }
+      })
+      .catch(err => res.status(400).json('wrong credentials'))
+  })
+
+app.post('/Register', (req, res) => {
+    const { email, name, password } = req.body;
+    if(!email || !password || !name){
+        return res.status(400).json('incorrect form submission');
+    }
+    const hash = bcrypt.hashSync(password);
+      db.transaction(trx => {
+        trx.insert({
+          hash: hash,
+          email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              res.json(user[0]);
+            })
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+      })
+      .catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id',(req, res)=>{
+    const {id}=req.params;
+    db.select('*').from('users').where({
+        id:id
+    }).then(user=>{
+        if(user.length){
+            res.json(user[0]);
+        }
+        else{
+            res.status(400).json('Not found')
+        }
+    }).catch(err=>res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+  })
+
+
+app.listen(3000,()=>{
+    console.log('on process');
+})
---
+++ face/facerecognition/src/App.js
@@ -0,0 +1,144 @@
+import React,{Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import './App.css'
+
+const app = new Clarifai.App({
+  apiKey: '3f583350f95948069728c8270459577d'
+ });
+
+
+const par={
+  particles: {
+    value:30,
+    density: {
+      density: {
+        enable: true,
+        value_area:800
+      }
+    }
+  }
+}
+
+const init={
+  input:'',
+  imageUrl:'',
+  box:{},
+  route:'SignIn',
+  isSignedIn:false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state=init;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+
+  calculateFaceLok=(data)=>{
+    const clarifaiFace=data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image=document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol:clarifaiFace.left_col*width,
+      topRow:clarifaiFace.top_row*height,
+      rightCol:width-(clarifaiFace.right_col*width),
+      bottomRow:height-(clarifaiFace.bottom_row*height)
+    }
+  }
+
+  displayFaceBox=(box)=>{
+    this.setState({box:box});
+  }
+
+  onInputChange=(event)=>{
+    this.setState({input:event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(res => res.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLok(response))
+      })
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange=(route)=>{
+    if(route === 'signout'){
+      this.setState(init)
+    }else if(route === 'home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles' params={par}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        {this.state.route==='home'
+          ? <div>
+              <Logo />
+              <Rank name={this.state.user.name}
+                entries={this.state.user.entries}/>
+              <ImageLinkForm
+                onInputChange={this.onInputChange}
+                onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            </div>
+          :(
+            this.state.route==='SignIn'
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
